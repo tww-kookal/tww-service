515a8b8f842137aac552bbfd4c80dcde7066c4ef
Pranav S <pranavam.s@gmail.com>
2025-08-11 17:19:52 +0530

    Need to have a has Value

--- app/utils.py
+++ app/utils.py
@@ -10,6 +10,7 @@ logger = logging.getLogger("tww.service.utils")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 #pwd_context = CryptContext(schemes=["bcrypt"],bcrypt__ident="2b", deprecated="auto")
+print(pwd_context.hash("@dmini$Tr@T0r@Tww#112"))
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
