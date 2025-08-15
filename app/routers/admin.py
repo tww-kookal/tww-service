c8063be011c21776753362b006dca44822ff4574
Pranav S <pranavam.s@gmail.com>
2025-08-15 12:48:03 +0530

    Added Admin Login

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -15,7 +15,7 @@ from .. import utils
 logger = logging.getLogger("tww.service.admin")
 
 router = APIRouter(
-    prefix="/n",  # all routes start with /api/v1/rooms
+    prefix="/",  # all routes start with /api/v1/rooms
     tags=["Admin"]    # OpenAPI grouping
 )
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
