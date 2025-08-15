46d537a06a7689b88d5cdbd91235c6dc73af7eee
Pranav S <pranavam.s@gmail.com>
2025-08-15 12:27:55 +0530

    Added Admin Login

--- app/routers/admin.py
+++ app/routers/admin.py
@@ -3,9 +3,11 @@ from typing import Annotated
 import traceback
 import logging
 from fastapi.security import OAuth2PasswordBearer
+from fastapi.security import OAuth2PasswordRequestForm
 from datetime import date
 from ..auth import get_current_user
-from ..biz import adminHelper as helper
+from ..biz import adminHelper as helper, usersHelper as userHelper
+
 from .. import utils
 
 ######## Logging ########
@@ -17,7 +19,18 @@ router = APIRouter(
 )
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-from pydantic import BaseModel
+@router.post("/login")
+def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user = userHelper.queryUser(form_data.username)
+
+    if not userHelper.validateUser(user, form_data.password):
+        raise HTTPException(status_code=400, detail="Invalid credentials")
+
+    access_token = utils.create_access_token(
+        data={"sub": user["username"]},
+        expires_delta=timedelta(minutes=config.settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/execute")
 async def execute(string: str, current_user: dict = Depends(get_current_user)):
